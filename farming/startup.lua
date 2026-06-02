555c9181f79281b4bfe9e0ff68c14795e1085fb6
MayaSapphire <71132181+MayaSapphire@users.noreply.github.com>
2026-06-02 10:49:37 -0500

    Fix bug, renamed files

--- farming/startup.lua
+++ farming/startup.lua
@@ -1,109 +1,39 @@
-local monitor = peripheral.find("monitor")
-monitor.clear()
+monitor = peripheral.find("monitor")
 monitor.setCursorPos(1,1)
-monitor.write("Reading config...")
+monitor.clear()
+ 
 local config = {}
 local file,err = loadfile("config.txt", "t", config)
 if file then
     file()
+    print("Farm Configuration")
+    print("Max crop age: "..config.maxCropAge)
+    print("Slot 1 minimum: "..config.slot1Minimum)
+    if config.skipForward then
+        print("Skip forward: ENABLED")
+    else
+        print("Skip forward: DISABLED")
+    end
+    print("Mode: "..config.mode)
+    print("Target block: "..config.targetBlock)
 else
-    monitor.clear()
-    monitor.setCursorPos(1,1)
     monitor.write("Config read error!")
-    monitor.setCursorPos(1,2)
-    monitor.write(err)
     error()
+    print(err)
 end
  
-monitor.clear()
-monitor.setCursorPos(1,1)
-monitor.write("Farming...")
-turtle.forward()
-turtle.forward()
-local blocksMined = 0
-function harvest()
-    local blockPresent, blockData = turtle.inspectDown()
-    if blockPresent == false then return true end
-    if config.mode == 'age' then
-        if blockData.state.age == nil then return true end
-        if blockData.state.age < config.maxCropAge then return true end
-        turtle.digDown()
-        placeSuccess, err = turtle.placeDown()
-        if placeSuccess == false then
-            print("Place failed: " .. err)
-        end
-    elseif config.mode == 'block' then
-        if blockData.name == config.targetBlock then
-            turtle.digDown()
-        end
-    end
-    blocksMined = blocksMined + 1
-end
- 
-function row()
-    for loops=1,8 do
-        harvest()
-        turtle.forward()
-    end
-    harvest()
-end
- 
-for loops=1,4 do
-    row()
-    turtle.turnLeft()
-    turtle.forward()
-    turtle.turnLeft()
-    row()
-    turtle.turnRight()
-    turtle.forward()
-    turtle.turnRight()
-end
-row()
-for loops=1,10 do
-    turtle.back()
-end
-turtle.turnRight()
-for loops=1,8 do
-    turtle.forward()
-end
-turtle.turnLeft()
-monitor.clear()
-monitor.setCursorPos(1,1)
-monitor.write("Depositing...")
-turtle.forward()
-turtle.turnRight()
-turtle.forward()
-turtle.forward()
-if config.skipForward == false then
-    turtle.forward()
-end
-itemsGained = 0
-while turtle.getItemCount() > config.slot1Minimum do
-   turtle.dropDown(1)
-   itemsGained = itemsGained + 1 
-end
-for selected=2, 16 do
-    turtle.select(selected)
-    while turtle.getItemCount() > 0 do
-        turtle.dropDown(1)
-        itemsGained = itemsGained + 1
+monitor.write("System ready.")
+while true do
+    :: continue ::
+    
+    local eventData = {os.pullEvent()}
+    local event = eventData[1]
+    if event == "monitor_touch" then
+        os.run({continuous=false},"farm.lua")
+    elseif event == "terminate" then 
+        monitor.setCursorPos(1,1)
+        monitor.clear()
+        monitor.write("Program terminated.")
     end
 end
-turtle.select(1)
-if config.skipForward == false then
-    turtle.back()
-end
-turtle.back()
-turtle.back()
-turtle.turnLeft()
-turtle.back()
-monitor.clear()
-monitor.setCursorPos(1,1)
-monitor.write("Farming complete!\n")
-monitor.setCursorPos(1,2)
-monitor.write(blocksMined .. " blocks mined.")
-monitor.setCursorPos(1,3)
-monitor.write(itemsGained .. " items gained.")
-monitor.setCursorPos(1,4)
-monitor.write("System ready.\n")
  
