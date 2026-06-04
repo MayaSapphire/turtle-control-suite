1e49c7a84e45f8a45f55559617f5813624f3cdef
MayaSapphire <71132181+MayaSapphire@users.noreply.github.com>
2026-06-03 21:13:42 -0500

    experimenting with update

--- farming/update.sh
+++ farming/update.sh
@@ -1,6 +1,6 @@
-delete farm.lua
-wget https://raw.githubusercontent.com/MayaSapphire/turtle-control-suite/refs/heads/main/farming/farm.lua
-delete startup.lua
-wget https://raw.githubusercontent.com/MayaSapphire/turtle-control-suite/refs/heads/main/farming/startup.lua
-delete update.sh
-wget https://raw.githubusercontent.com/MayaSapphire/turtle-control-suite/refs/heads/main/farming/update.sh
+shell.execute("delete", "farm.lua")
+shell.execute("wget", "https://raw.githubusercontent.com/MayaSapphire/turtle-control-suite/refs/heads/main/farming/farm.lua")
+shell.execute("delete", "startup.lua")
+shell.execute("wget", "https://raw.githubusercontent.com/MayaSapphire/turtle-control-suite/refs/heads/main/farming/startup.lua")
+shell.execute("delete", "update.sh")
+shell.execute("wget", "https://raw.githubusercontent.com/MayaSapphire/turtle-control-suite/refs/heads/main/farming/update.sh")
