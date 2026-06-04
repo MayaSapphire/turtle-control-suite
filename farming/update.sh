52b8f6a008d398a9293633b68f7efdfbc2d37f77
MayaSapphire <71132181+MayaSapphire@users.noreply.github.com>
2026-06-03 21:07:29 -0500

    add update functionality

---
+++ farming/update.sh
@@ -0,0 +1,6 @@
+delete farm.lua
+wget https://raw.githubusercontent.com/MayaSapphire/turtle-control-suite/refs/heads/main/farming/farm.lua
+delete startup.lua
+wget https://raw.githubusercontent.com/MayaSapphire/turtle-control-suite/refs/heads/main/farming/startup.lua
+delete update.sh
+wget https://raw.githubusercontent.com/MayaSapphire/turtle-control-suite/refs/heads/main/farming/update.sh
